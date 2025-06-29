ac14aa5107ceaff5bd97b7d9ad63e2757c14e713
Ryan Tsai <90505632+requiemdev@users.noreply.github.com>
2025-06-29 17:49:22 +1200

    Implement RequestService and test API calls

---
+++ main.ts
@@ -0,0 +1,21 @@
+import { RequestService } from "./utils/requests/RequestService.ts";
+import config from "./config.json";
+
+const reqService = RequestService.getInstance(
+  "https://docs.saddlebagexchange.com"
+);
+
+async function main() {
+  const payload = {
+    server: "Sophia",
+    time_period: 168,
+    sales_amount: 3,
+    average_price: 10000,
+    filters: [56, 65, 66, 67, 68, 69, 70, 71, 72, 81, 82],
+    sort_by: "marketValue",
+  };
+  const response = await reqService.post("/api/ffxivmarketshare", payload);
+  console.log(response.data);
+}
+
+main();
---
+++ utils/requests/RequestService.ts
@@ -0,0 +1,72 @@
+import axios, { AxiosInstance, AxiosResponse } from "axios";
+
+export class RequestService {
+  private url: string;
+  private static instance: RequestService;
+  private axiosInstance: AxiosInstance;
+
+  constructor(url: string) {
+    this.url = url;
+    this.axiosInstance = axios.create({
+      baseURL: this.url,
+      timeout: 1000,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
+  public static getInstance(url: string): RequestService {
+    if (!RequestService.instance) {
+      RequestService.instance = new RequestService(url);
+    }
+    return RequestService.instance;
+  }
+
+  public async get(
+    endpoint: string,
+    param: object
+  ): Promise<AxiosResponse<any>> {
+    const response = await this.axiosInstance
+      .get(endpoint, param)
+      .catch((error) => {
+        console.log(error.message);
+        return error.toJSON();
+      });
+    return response;
+  }
+
+  public async post(
+    endpoint: string,
+    body: object
+  ): Promise<AxiosResponse<any>> {
+    const response = await this.axiosInstance
+      .post(endpoint, body)
+      .catch((error) => {
+        console.log(error.message);
+        return error.toJSON();
+      });
+    return response;
+  }
+
+  public async put(
+    endpoint: string,
+    body: object
+  ): Promise<AxiosResponse<any>> {
+    const response = await this.axiosInstance
+      .put(endpoint, body)
+      .catch((error) => {
+        console.log(error.message);
+        return error.toJSON();
+      });
+    return response;
+  }
+
+  set baseUrl(url: string) {
+    this.url = url;
+  }
+
+  get baseUrl(): string {
+    return this.url;
+  }
+}
